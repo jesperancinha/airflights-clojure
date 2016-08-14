3d63b0fa0e93a0d0e884073eae02ecec9da08f9c
Joao FSE <jofisaes@hotmail.com>
2016-08-14 16:51:18 +0200

    SZ-N Adds some test functions

--- src/airflights_clojure/core.clj
+++ src/airflights_clojure/core.clj
@@ -1,6 +1,22 @@
 (ns airflights-clojure.core)
 
+((defn -main
+   "I don't do a whole lot."
+   [& args]
+   (println "Hello, Worldy!")))
+
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn i-add-numbers
+   "I actually add two numbers"
+    [x y]
+   (+ x y)
+ )
+(defn i-add-numbers-overloaded
+  ([x y] (+ x y))
+  ([x y & args]
+   (reduce + (i-add-numbers-overloaded x y) args))
+  )
